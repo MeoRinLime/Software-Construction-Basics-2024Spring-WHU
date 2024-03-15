cd3501bc89abe1331abcc70eb872862211807824
Lime_omie <lime_omie@whu.edu.cn>
2024-03-15 11:01:12 +0800

    Assignment4

---
+++ Assignment4/task1/Program.cs
@@ -0,0 +1,85 @@
+﻿//链表结点
+public class Node<T>
+{
+    public Node<T>? Next { get; set; }
+    public T? Data { get; set; }
+
+    public Node(T data)
+    {
+        Next = null;
+        Data = data;
+    }
+}
+
+//泛型链表类
+public class GenericList<T>
+{
+    private Node<T>? head;
+    private Node<T>? tail;
+
+    public GenericList()
+    {
+        head = tail = null;
+    }
+
+    public Node<T>? Head
+    {
+        get => head;
+    }
+
+    public void Add(T t)
+    {
+        Node<T> n = new Node<T>(t);
+        if (tail == null)
+        {
+            head = tail = n;
+        }
+        else
+        {
+            tail.Next = n;
+            tail = n;
+        }
+    }
+
+    public void ForEach(Action<T> action)
+    {
+        Node<T>? current = head;
+        while (current != null)
+        {
+            action(current.Data);
+            current = current.Next;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        GenericList<int> list = new GenericList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(9);
+        list.Add(8);
+        list.Add(7);
+
+        // 打印链表元素
+        list.ForEach(item => Console.WriteLine(item));
+
+        // 求最大值
+        int max = int.MinValue;
+        list.ForEach(item => { if (item > max) max = item; });
+        Console.WriteLine($"Max value: {max}");
+
+        // 求最小值
+        int min = int.MaxValue;
+        list.ForEach(item => { if (item < min) min = item; });
+        Console.WriteLine($"Min value: {min}");
+
+        // 求和
+        int sum = 0;
+        list.ForEach(item => sum += item);
+        Console.WriteLine($"Sum: {sum}");
+    }
+}
---
+++ Assignment4/task2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+public class Clock
+{
+    // 定义委托
+    public delegate void ClockHandler(object sender, EventArgs e);
+
+    // 定义事件
+    public event ClockHandler? Tick;
+    public event ClockHandler? Alarm;
+
+    // 触发事件的方法
+    protected virtual void OnTick(EventArgs e)
+    {
+        Tick?.Invoke(this, e);
+    }
+
+    protected virtual void OnAlarm(EventArgs e)
+    {
+        Alarm?.Invoke(this, e);
+    }
+
+    // 闹钟运行方法
+    public void Run()
+    {
+        while (true)
+        {
+            Thread.Sleep(1000); // 每秒Tick一次
+            OnTick(EventArgs.Empty);
+
+            DateTime now = DateTime.Now;
+            if (now.Second % 10 == 0 ) // 每10秒Alarm一次
+            {
+                OnAlarm(EventArgs.Empty);
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Clock clock = new Clock();
+
+        // 注册事件
+        clock.Tick += (sender, e) => Console.WriteLine("Tick...");
+        clock.Alarm += (sender, e) => Console.WriteLine("Alarm!");
+
+        // 运行闹钟
+        clock.Run();
+    }
+}
